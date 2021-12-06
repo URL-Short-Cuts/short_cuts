95b052fc47808a7cf55d3559fca9ec1dc6b4b148
Niall Byrne <niall@niallbyrne.ca>
2021-12-06 15:59:29 -0500

    feat(COMPONENTS.FORMS): add url generation form

---
+++ src/components/forms/url/tests/url.form.component.test.tsx
@@ -0,0 +1,116 @@
+import { Flex } from "@chakra-ui/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import checkMockCall from "../../../../tests/fixtures/mock.component.call";
+import StyledButton from "../../../button/button.standard/button.standard.component";
+import FormComponent from "../url.form.component";
+
+jest.mock("../../../button/button.standard/button.standard.component", () => {
+  const Original = jest.requireActual(
+    "../../../button/button.standard/button.standard.component"
+  ).default;
+  return jest.fn((props) => <Original {...props} />);
+});
+
+jest.mock("@chakra-ui/react", () => {
+  const {
+    factoryInstance,
+  } = require("../../../../tests/fixtures/mock.chakra.react.factory.class");
+  return factoryInstance.create(["Flex"]);
+});
+
+describe("FormComponent", () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  const mockT = jest.fn((arg: string) => `t(${arg})`);
+  const mockValidate = jest.fn();
+  const mockSubmit = jest.fn();
+
+  const arrange = () => {
+    render(
+      <FormComponent
+        t={mockT}
+        validateURL={mockValidate}
+        handleSubmit={mockSubmit}
+      />
+    );
+  };
+
+  it("should call Flex as expected to center content", () => {
+    arrange();
+    expect(Flex).toBeCalledTimes(1);
+    checkMockCall(Flex, { justify: "center", maxWidth: "700px" });
+  });
+
+  it("should display the correct url field label", async () => {
+    arrange();
+    expect(
+      await screen.findByText("t(url.fields.url.fieldLabel)")
+    ).toBeTruthy();
+  });
+
+  it("should display the correct url placeholder text", async () => {
+    arrange();
+    expect(
+      await screen.findByPlaceholderText("t(url.fields.url.fieldPlaceHolder)")
+    ).toBeTruthy();
+  });
+
+  it("should display the correct button text", async () => {
+    arrange();
+    expect(await screen.findByText("t(url.buttonText)")).toBeTruthy();
+  });
+
+  it("should call StyledInput once", async () => {
+    arrange();
+    const inputField = (await screen.findByPlaceholderText(
+      "t(url.fields.url.fieldPlaceHolder)"
+    )) as HTMLInputElement & { ref: string };
+    expect(inputField.id).toBe("url");
+    expect(inputField.placeholder).toBe("t(url.fields.url.fieldPlaceHolder)");
+  });
+
+  it("should call Button with the correct props", async () => {
+    arrange();
+    expect(StyledButton).toBeCalledTimes(1);
+    checkMockCall(StyledButton, {
+      width: ["50px", "100px", "100px"],
+      analyticsName: "URL: generate shortcut",
+      isLoading: false,
+      mb: 12,
+      mt: 8,
+      ml: 3,
+      type: "submit",
+    });
+  });
+
+  describe("when text is entered", () => {
+    beforeEach(async () => {
+      jest.clearAllMocks();
+      mockValidate.mockImplementation(() => undefined);
+      arrange();
+      const input = await screen.findByPlaceholderText(
+        "t(url.fields.url.fieldPlaceHolder)"
+      );
+      await waitFor(() => {
+        fireEvent.change(input, { target: { value: "field_value" } });
+      });
+    });
+
+    describe("when submit is pressed", () => {
+      let button: HTMLElement;
+
+      beforeEach(async () => {
+        button = await screen.findByText("t(url.buttonText)");
+        await waitFor(() => {
+          fireEvent.focus(button);
+          fireEvent.click(button);
+        });
+      });
+
+      it("should call the handleSubmit function as expected", () => {
+        expect(mockValidate).toBeCalledTimes(2);
+        expect(mockSubmit).toBeCalledTimes(1);
+      });
+    });
+  });
+});
---
+++ src/components/forms/url/tests/url.integration.test.tsx
@@ -0,0 +1,132 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import formTranslations from "../../../../../public/locales/en/forms.json";
+import mockRouter from "../../../../tests/fixtures/mock.router";
+import tLookup from "../../../../tests/fixtures/mock.translation";
+import FormUI from "../url.ui.component";
+
+jest.mock("next/router", () => ({
+  __esModule: true,
+  useRouter: () => mockRouter,
+}));
+
+describe("FormIntegrationTest", () => {
+  let enteredURL: string;
+  const mockT = jest.fn((key) => tLookup(key, formTranslations));
+  const mockTitle = "mockTitle";
+  const mockRoute = "/mock/route";
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    arrange();
+  });
+
+  const arrange = () => {
+    render(<FormUI t={mockT} title={mockTitle} route={mockRoute} />);
+  };
+
+  it("should display the correct title", async () => {
+    expect(await screen.findByText(mockTitle)).toBeTruthy();
+  });
+
+  it("should display the correct field label", async () => {
+    expect(
+      await screen.findByText(formTranslations.url.fields.url.fieldLabel)
+    ).toBeTruthy();
+  });
+
+  it("should display the correct input placeholder", async () => {
+    expect(
+      await screen.findByPlaceholderText(
+        formTranslations.url.fields.url.fieldPlaceHolder
+      )
+    ).toBeTruthy();
+  });
+
+  it("should display the correct buttonText", async () => {
+    expect(
+      await screen.findByText(formTranslations.url.buttonText)
+    ).toBeTruthy();
+  });
+
+  describe("given no url", () => {
+    beforeEach(async () => {
+      const element = screen.getByPlaceholderText(
+        formTranslations.url.fields.url.fieldPlaceHolder
+      ) as HTMLInputElement;
+      expect(element.value).toBe("");
+    });
+
+    describe("when search is pressed", () => {
+      beforeEach(async () => {
+        const element = screen.getByText(formTranslations.url.buttonText);
+        await waitFor(() => fireEvent.click(element));
+      });
+
+      it("should show an error message", async () => {
+        expect(
+          await screen.findByText(
+            formTranslations.url.fields.url.errors.required
+          )
+        ).toBeTruthy();
+      });
+    });
+  });
+
+  describe("given an invalid url", () => {
+    beforeEach(async () => {
+      enteredURL = "invalid";
+      const element = screen.getByPlaceholderText(
+        formTranslations.url.fields.url.fieldPlaceHolder
+      ) as HTMLInputElement;
+      expect(element.value).toBe("");
+      await waitFor(() =>
+        fireEvent.change(element, { target: { value: enteredURL } })
+      );
+    });
+
+    describe("when search is pressed", () => {
+      beforeEach(async () => {
+        const element = screen.getByText(formTranslations.url.buttonText);
+        await waitFor(() => fireEvent.click(element));
+      });
+
+      it("should show an error message", async () => {
+        expect(
+          await screen.findByText(
+            formTranslations.url.fields.url.errors.invalid
+          )
+        ).toBeTruthy();
+      });
+    });
+  });
+
+  describe("given a valid username", () => {
+    beforeEach(async () => {
+      enteredURL = "http://yahoo.com";
+      const element = screen.getByPlaceholderText(
+        formTranslations.url.fields.url.fieldPlaceHolder
+      );
+      await waitFor(() =>
+        fireEvent.change(element, { target: { value: enteredURL } })
+      );
+    });
+
+    describe("when search is pressed", () => {
+      beforeEach(async () => {
+        const element = screen.getByText(formTranslations.url.buttonText);
+        await waitFor(() => fireEvent.click(element));
+      });
+
+      it("should redirect to the expected page", () => {
+        const params = {
+          url: enteredURL,
+        };
+        const query = new URLSearchParams(params);
+        expect(mockRouter.push).toBeCalledTimes(1);
+        expect(mockRouter.push).toBeCalledWith(
+          `${mockRoute}?${query.toString()}`
+        );
+      });
+    });
+  });
+});
