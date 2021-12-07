973bef28b4df5b6ed442cc5e0064ba9571c2b55e
Niall Byrne <niall@niallbyrne.ca>
2021-12-07 15:58:14 -0500

    feat(BACKEND): url api endpoint base class

---
+++ src/backend/api/endpoint.logger.ts
@@ -0,0 +1,31 @@
+import type { APIRequest } from "../../types/api/request.d";
+import type { NextApiResponse } from "next";
+
+const logger = (req: APIRequest, res: NextApiResponse, next: () => void) => {
+  const remoteIp = getRemoteIpAddress(req);
+
+  let message = `${remoteIp} `;
+  message += `${req.method} `;
+  message += `${req.url} `;
+  message += `${res.statusCode} `;
+  message += `${req.headers["content-length"]} `;
+  message += `${req.headers["referer"]} `;
+  message += `${req.headers["user-agent"]}`;
+  if (req.error) message += ` (Errors: ${req.error})`;
+
+  console.log(message);
+
+  next();
+};
+
+const getRemoteIpAddress = (req: APIRequest) => {
+  if (req.socket?.remoteAddress) return req.socket.remoteAddress;
+
+  const fromForwardedHeader = req.headers["x-forwarded-for"];
+  if (typeof fromForwardedHeader === "string")
+    return fromForwardedHeader.split(",")[0];
+  if (Array.isArray(fromForwardedHeader)) return fromForwardedHeader[0];
+  return fromForwardedHeader;
+};
+
+export default logger;
