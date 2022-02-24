6bc78c63a20f05a9c9d066a8a70d27112efba361
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-02-23 21:16:23 -0500

    build(DOCKER): dockerize application for heroku

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 const { i18n } = require("./next-i18next.config");
 
 module.exports = {
-  target: "experimental-serverless-trace",
   i18n,
-  env: {
-    HOSTNAME: process.env.HOSTNAME,
-    SHORT_CUTS_DYNAMO_DB_TABLE: process.env.SHORT_CUTS_DYNAMO_DB_TABLE,
-    SHORT_CUTS_AWS_REGION: process.env.SHORT_CUTS_AWS_REGION,
-    SHORT_CUTS_AWS_ACCESS_KEY: process.env.SHORT_CUTS_AWS_ACCESS_KEY,
-    SHORT_CUTS_AWS_SECRET_ACCESS_KEY:
-      process.env.SHORT_CUTS_AWS_SECRET_ACCESS_KEY,
-    SHORT_CUTS_URL_BUCKET: process.env.SHORT_CUTS_URL_BUCKET,
+  experimental: {
+    outputStandalone: true,
   },
 };
